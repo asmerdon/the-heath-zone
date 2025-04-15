103d6c092840407612b242753a15727a19977be9
Alex Smerdon <alexsmerdon7@gmail.com>
2025-04-15 18:32:25 +0100

    Updated gallery buttons; changed name of renders

--- src/collections/artwork.js
+++ src/collections/artwork.js
@@ -1,24 +1,24 @@
 export const artworkCollections = [
   {
-    name: 'Into the Light',
+    name: 'Orbs',
     description: 'A series of renders created during March 2025.',
     items: [
       {
         type: 'image',
-        url: 'artwork/into-the-light/1.png'
+        url: 'artwork/orbs/1.png'
       },
       {
         type: 'image',
-        url: 'artwork/into-the-light/2.png'
+        url: 'artwork/orbs/2.png'
       },
       {
         type: 'image',
-        url: 'artwork/into-the-light/3.png'
+        url: 'artwork/orbs/3.png'
       },
       {
         type: 'video',
-        url: 'artwork/into-the-light/4.mp4',
-        thumbnail: 'artwork/into-the-light/1.png'
+        url: 'artwork/orbs/4.mp4',
+        thumbnail: 'artwork/orbs/1.png'
       }
     ]
   },
